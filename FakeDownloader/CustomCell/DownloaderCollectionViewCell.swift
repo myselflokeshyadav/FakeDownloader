8af5d722bd2079bca83de6947a8214622c0aa998
LOKESH YADAV <lokesh1801@gmail.com>
2019-05-18 10:29:04 +0530

    first commit

---
+++ FakeDownloader/CustomCell/DownloaderCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DownloaderCollectionViewCell.swift
+//  FakeDownloader
+//
+//  Created by 甘忠达 on 5/17/19.
+//  Copyright © 2019 Lokesh Yadav. All rights reserved.
+//
+
+import UIKit
+
+class DownloaderCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FakeDownloader/Model/DownloaderModel.swift
@@ -0,0 +1,9 @@
+//
+//  DownloaderModel.swift
+//  FakeDownloader
+//
+//  Created by 甘忠达 on 5/17/19.
+//  Copyright © 2019 Lokesh Yadav. All rights reserved.
+//
+
+import Foundation
